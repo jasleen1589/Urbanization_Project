13294647772cc192bf80eca2df88fff693c16691
ashleevo <ashleevo@Ashlees-MacBook-Pro.local>
2024-02-15 19:15:15 -0800

    adding and updating

---
+++ Homeless JavaScript Pie/data.js
@@ -0,0 +1,20 @@
+let data = {
+    lower_tier: {
+        "Violent Crime": 434.04,
+        "Property Crime": 1759.43
+    },
+    middle_tier: {
+        "Violent Crime": 398.16,
+        "Property Crime": 2074.12
+    },
+    upper_middle_tier: {
+        "Violent Crime": 378.1,
+        "Property Crime": 1310.06
+    },
+    upper_tier: {
+        "Violent Crime": 508.8,
+        "Property Crime": 3419.05
+    }
+    
+    
+};
